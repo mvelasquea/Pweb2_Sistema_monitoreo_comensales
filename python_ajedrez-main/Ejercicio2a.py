2d4dc43b0c150a719b02f5615e31a98713b74519
mvelasquea <144069262+mvelasquea@users.noreply.github.com>
2024-06-02 00:04:58 -0500

    cambio

--- python_ajedrez-main/Ejercicio2a.py
+++ python_ajedrez-main/Ejercicio2a.py
@@ -9,9 +9,4 @@ tab = knight
 tab = Picture.join(tab, Picture.negative(knight)) 
 #llama ala segunda columna 
 draw(Picture.up(Picture.negative(tab), tab))
-=======
-tab = knight
-tab = Picture.join(tab, Picture.negative(knight))
-tab = Picture.up(Picture.negative(tab), tab)
-draw(tab)
->>>>>>> df8f628231e271c37e75bf880b129d6a81d50828
+
